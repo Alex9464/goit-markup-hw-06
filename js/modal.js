d4def6730617c3988f57bee38fbc6cd344618bdb
Alex <redj9464@gmail.com>
2024-07-29 16:21:46 +0300

    fix modal opener

--- js/modal.js
+++ js/modal.js
@@ -3,21 +3,22 @@ const refs = {
   closeModalBtn: document.querySelector('[data-close-modal]'),
   backdrop: document.querySelector('[data-backdrop]'),
 };
+document.body.classList.remove('modal-open');
 
 refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
-
 refs.backdrop.addEventListener('click', logBackdropClick);
 
 function toggleModal() {
-  refs.backdrop.classList.toggle('is-hidden');
-  document.body.classList.toggle('modal-open');
+  const isHidden = refs.backdrop.classList.toggle('is-hidden');
+  if (isHidden) {
+    document.body.classList.remove('modal-open');
+  } else {
+    document.body.classList.add('modal-open');
+  }
 }
 
 function logBackdropClick() {
   console.log('клик в бекдроп');
-  if (event.target === refs.backdrop) {
-    toggleModal();
-  }
 }
 
